eea459fae40c2da8364b971c6f95a4622827724f
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-04-14 16:15:23 +0200

    Create Solution.cs

---
+++ Solution.cs
@@ -0,0 +1,34 @@
+
+using System;
+
+public class Solution
+{
+    public int CountDivisibleSubstrings(string word)
+    {
+        int[] prefixSum = new int[word.Length];
+        prefixSum[0] = letterToButtonDigit(word[0]);
+        int countDivisibleSubstrings = 1;
+
+        for (int front = 1; front < word.Length; ++front)
+        {
+            prefixSum[front] = prefixSum[front - 1] + letterToButtonDigit(word[front]);
+            countDivisibleSubstrings += (prefixSum[front] % (front + 1) == 0) ? 1 : 0;
+
+            for (int back = 0; back < front; ++back)
+            {
+                countDivisibleSubstrings += ((prefixSum[front] - prefixSum[back]) % (front - back) == 0) ? 1 : 0;
+            }
+        }
+
+        return countDivisibleSubstrings;
+    }
+
+    /*
+     a,b => 1 | c,d,e => 2 | f,g,h => 3 | i,j,k => 4 | l,m,n => 5
+     o,p,q => 6 | r,s,t => 7 | u,v,w => 8 | x,y,z => 9
+     */
+    private int letterToButtonDigit(char letter)
+    {
+        return (letter - 'a' + 4) / 3;
+    }
+}
